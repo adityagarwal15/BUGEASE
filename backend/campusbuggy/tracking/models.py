8512dc96ec32fdbcc08e49e1fe4ea31d226aec4f
ishaan-aggarwal <154077101+ishaan-aggarwal@users.noreply.github.com>
2025-04-13 20:01:47 +0530

    Add capacity field to Buggy model and create migration

--- backend/campusbuggy/tracking/models.py
+++ backend/campusbuggy/tracking/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 # For buggy 
 class Buggy(models.Model):
     number_plate = models.CharField(max_length=20, unique=True)
+    capacity = models.PositiveIntegerField()
     assigned_driver = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
