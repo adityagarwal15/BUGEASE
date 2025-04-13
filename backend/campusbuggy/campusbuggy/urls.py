e8bb194e3b006ec786d83978b2335e67cabaf701
ishaan-aggarwal <154077101+ishaan-aggarwal@users.noreply.github.com>
2025-04-13 21:22:08 +0530

    Fix URL pattern for tracking API by adding missing trailing slash

--- backend/campusbuggy/campusbuggy/urls.py
+++ backend/campusbuggy/campusbuggy/urls.py
@@ -40,7 +40,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/user/', include('users.urls')),
-    path('api/tracking', include('tracking.urls')),
+    path('api/tracking/', include('tracking.urls')),
     # Swagger UI and ReDoc
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
